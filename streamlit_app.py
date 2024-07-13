15250e9185b78b8aa1e0e1b6361dad49726e2972
Jessica Smith <12jessicasmith34@gmail.com>
2024-07-12 22:42:44 -0500

    runs otf!

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,15 +3,44 @@ import asyncio
 import streamlit as st
 from otf_api import Api
 
+st.set_page_config(page_title="OTF API")
+st.session_state.username = ""
+st.session_state.password = ""
+
+empty_form = st.empty()
+login_form = empty_form.form("my_form")
+username = login_form.text_input("Username")
+password = login_form.text_input("Password", type="password")
+submit = login_form.form_submit_button("Submit")
+
+if submit:
+    st.session_state.username = username
+    st.session_state.password = password
+    empty_form.empty()
+
 
 async def main():
     # Show the page title and description.
-    st.set_page_config(page_title="Movies dataset", page_icon="🎬")
 
-    username = st.text_input("Username")
-    password = st.text_input("Password", type="password")
+    st.write("# Welcome to the OTF API")
+    st.write("Login to the OTF API to get your member and home studio information")
 
-    otf = await Api.create(username, password)
+    while st.session_state.password == "":
+        await asyncio.sleep(1)
+
+    empty = st.empty()
+    empty.status("Logging in...")
+    try:
+        otf = await Api.create(st.session_state.username, st.session_state.password)
+
+    except Exception as e:
+        st.write(f"Error: {e}")
+        return
+
+    empty.status("Logged in!", state="complete")
+    empty.empty()
+
+    st.write(f"Thanks for logging in {otf.member.first_name}!")
 
     st.text(otf.member)
     st.text(otf.home_studio)
